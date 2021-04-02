abe17be441e48fa30a18ef773984cb69c93a765e
Fred McWilliams <fredmcwilliams92@gmail.com>
2021-04-01 21:09:39 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,14 +28,16 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
+    // generate random damage value based on players attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -53,8 +55,10 @@ var fight = function(enemyName) {
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     }
 
+    // generate random damage value based on enemies attack damage
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -80,13 +84,12 @@ var startGame = function() {
     if (playerHealth > 0) {
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       fight(pickedEnemyName);
       // if were not at the last enemy in the array
       if (playerHealth > 0 && i < enemyNames.length - 1) {
         // ask the player if they want to enter the shop
         var storeConfirm = window.confirm("the fight is over, visit the store before the next round?");
-
         // if yes, take them to the store() function
         if (storeConfirm) {
         shop();
@@ -168,6 +171,14 @@ var shop = function() {
     break;
   }
 };
+
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
+
 
 // start the game when the page loads
 startGame();
